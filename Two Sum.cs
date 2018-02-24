5edbe4a1e1e083fc3316702619de61f5fe42ed90
heshameraqi <hesham.eraqi@gmail.com>
2018-02-25 00:01:16 +0200

    Update Two Sum.cs

--- Two Sum.cs
+++ Two Sum.cs
@@ -1,4 +1,4 @@
-################################################################################
+/*################################################################################
 #
 # Copyright (c) 2018
 # Authors:
@@ -7,7 +7,7 @@
 # Problem:
 # Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
-################################################################################
+################################################################################*/
 
 using System.Collections.Generic; // For Lists and Dictionaries
 
