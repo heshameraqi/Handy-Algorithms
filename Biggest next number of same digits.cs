a00efa4578df00faed341c43d438250f39c22793
heshameraqi <hesham.eraqi@gmail.com>
2018-03-16 05:31:23 +0200

    Minor edits in example

--- Biggest next number of same digits.cs
+++ Biggest next number of same digits.cs
@@ -12,7 +12,7 @@ using System.Text; // For StringBuilder
 class Solution
 {
     static int FindPivot(string num) {
-        for (int i=num.Length-1; i>=1; i--) {
+        for (int i=num.Length-1; i>=1; i--) { // From right to left
             if ( Convert.ToInt32(num[i-1]) < Convert.ToInt32(num[i]) )
                 return i-1;
         }
@@ -22,11 +22,11 @@ class Solution
     static void Main(string[] args) // If number is 9876 it's the result already
     {
         //string num = Console.ReadLine();
-        string num = "98791"; // 53921->59123 98791->98971
+        string num = "98753941"; // 98753941->98754931 98791->98971
         StringBuilder result = new StringBuilder(num);
         int p_idx = Solution.FindPivot(num);
         if (p_idx != -1) {
-            // Get lowest_biggest_right_idx
+            // Get lowest_bigger_right_idx
             int lowest_biggest_right_idx = p_idx+1;
             for (int i = p_idx+2; i < num.Length; i++)
                 if (Convert.ToInt32(num[i]) > Convert.ToInt32(num[p_idx]) && 
@@ -43,7 +43,6 @@ class Solution
             
             // Sort
             string toSort = result.ToString().Substring(lowest_biggest_right_idx);
-            
             char[] toSort_array = toSort.ToCharArray();
             Array.Sort(toSort_array);
             toSort = new string(toSort_array);
