c01e996c208ce64e7786176421f9745702ee00f6
heshameraqi <hesham.eraqi@gmail.com>
2020-09-22 16:57:32 +0200

    Update Undirected Graph Traversal using BFS or DFS non-recursive.cs

--- Undirected Graph Traversal using BFS or DFS non-recursive.cs
+++ Undirected Graph Traversal using BFS or DFS non-recursive.cs
@@ -34,7 +34,7 @@ class Solution
             while (searchQueue.Count != 0) {
                 k = searchQueue.Dequeue();
                 order[k] = ++current_order;
-                for (int i = 0; i<data.Length; i++) //left to right (more accurate than the opposite)
+                for (int i = 0; i<data.Length; i++) //left to right
                     if (Adj_Matrix[i,k] && order[i] == 0) { // 0:unseen
                         searchQueue.Enqueue(i);
                         order[i] = -1; // -1:seen but waiting it's value
@@ -48,12 +48,11 @@ class Solution
             while (searchStack.Count != 0) {
                 k = searchStack.Pop();
                 order[k] = ++current_order;
-                for (int i = data.Length-1; i>=0; i--) //right to left
+                for (int i = data.Length-1; i>=0; i--) //right to left (more accurate than the opposite)
                     if (Adj_Matrix[i,k] && order[i] == 0) { // 0:unseen
                         searchStack.Push(i);
                         order[i] = -1; // -1:seen but waiting it's value
                     }
-
             }
         }
 
