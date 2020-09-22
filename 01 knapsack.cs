1cfe9b376641947a1ff7cc562d49a6106a9a5b51
heshameraqi <hesham.eraqi@gmail.com>
2020-09-22 15:51:39 +0200

    Update 01 knapsack.cs

--- 01 knapsack.cs
+++ 01 knapsack.cs
@@ -14,34 +14,43 @@ class Solution
         return (x >= y) ? x : y;
     }
     
-    static int knapsack_recursive(int[] w, int[] v, int n, int m) {
-        if (n > 0) {
-            int p = knapsack_recursive(w, v, n-1, m);
-            if (w[n-1] <= m)
-                p = Max(p, knapsack_recursive(w, v, n-1, m-w[n-1])+v[n-1]);
-            return p;
+    static int knapsack_recursive(int[] w, int[] v, int n, int c) {
+        // n is number of remaining items to decide to put or not
+        // c is the remaning weight capacity
+        if (n > 0 && c > 0) {
+            int current_v = knapsack_recursive(w, v, n-1, c); // value for not putting the item given optimal decisions before
+            if (w[n-1] <= c)  // I can take that item
+                current_v = Max(current_v, // not taking it
+                        knapsack_recursive(w, v, n-1, c-w[n-1]) + v[n-1]); // taking it
+            return current_v;
         }
         else
             return 0;
     }
     
-    static int knapsack_dynamicProg(int[] w, int[] v, int m) {
-        int[,] p = new int[w.Length+1,m+1]; // By default 0's
-        for (int i=1; i<p.GetLength(0); i++)
-            for (int j=0; j<p.GetLength(1); j++) {
-                p[i,j] = p[i-1,j];
-                if (j >= w[i-1]) p[i,j] = Max(p[i-1,j],  p[i-1,j-w[i-1]]+v[i-1]);
+    static int knapsack_dynamicProg(int[] w, int[] v, int c) {
+        int[,] current_v = new int[w.Length+1,c+1]; // By default 0's
+        int item_i_weight = 0;
+        int item_i_value = 0;
+        for (int i=1; i<current_v.GetLength(0); i++) {
+            item_i_weight = w[i-1];
+            item_i_value = v[i-1];
+            for (int j=1; j<current_v.GetLength(1); j++) {
+                if (j >= item_i_weight) current_v[i,j] = Max(current_v[i-1,j], current_v[i-1,j-item_i_weight]+item_i_value); // Take it or not
+                else
+                    current_v[i,j] = current_v[i-1,j]; // Won't take it
             }
-        return p[w.Length,m];
+        }
+        return current_v[w.Length,c];
     }
     
     static void Main(string[] args)
     {
         int[] w = {2,1,3,2};
         int[] v = {12,10,20,15};
-        int m = 5;
+        int c = 5;
         
-        Console.WriteLine(knapsack_recursive(w,v,w.Length,m)); // O(n^2)
-        Console.WriteLine(knapsack_dynamicProg(w,v,m));  // // O(nm), linear in n
+        Console.WriteLine(knapsack_recursive(w,v,w.Length,c)); // O(n^2)
+        Console.WriteLine(knapsack_dynamicProg(w,v,c));  // // O(nc), linear in n
     }
 }
