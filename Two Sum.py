4dad155a85a11df0670840e12dcfed1bde242658
heshameraqi <hesham.eraqi@gmail.com>
2021-01-06 21:25:35 +0200

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -12,6 +12,8 @@
 x = [2, 4, 1, 5, 10, 6]
 target = 10
 
+# A hash table (dictionary in python) (if we don't want to report the indices, we don't need to store any values in our hash table which means it's better be a hash set)
+#   has set in python is a set like: my_set = {1, 2, 3}
 h = {} #value (because later I ask: does that value exist in the array in O(1)) to index
 for i in range(len(x)):
     h[x[i]] = i
