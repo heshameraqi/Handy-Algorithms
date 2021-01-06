797dbc63d4dd5bd517714ea3ae2f62bcb7d362ef
heshameraqi <hesham.eraqi@gmail.com>
2021-01-07 01:14:09 +0200

    Update fit_to_circle.py

--- fit_to_circle.py
+++ fit_to_circle.py
@@ -1,18 +1,21 @@
-import matplotlib.pyplot as plt
 from scipy.optimize import leastsq
 import numpy as np
 
 x = [-5, 0, 5, 0]
 y = [0, 5, 0, -6]
 
-plt.plot(x, y, '*')
+def losses(c):  # Returns array: loss for each point
+  dist = np.sqrt((x-c[0])**2 + (y-c[1])**2)  # Array
+  r = np.mean(dist)
+  return dist - r  # can't take the mean of this because scipy,optimize.leastsq needs it longer than the number of parameters to optimize (2 in our case)
 
-def losses(c): # Returns array: loss for each point
-  dist = np.sqrt((x-c[0])**2 + (y-c[1])**2) # Array
-  return dist - np.mean(dist)
-
+# For any centre, we can calculate the best r (the mean of distances from that centre)
 centre_intial = np.mean(x), np.mean(y)
-centre, _ = leastsq(losses, centre_intial) # Losses to minize their squres error
-r = np.sqrt((x-centre[0])**2 + (y-centre[1])**2).mean()
+centre_optimized, _ = leastsq(losses, centre_intial)  # Losses to minimize their squares error
+r = np.sqrt((x-centre_optimized[0])**2 + (y-centre_optimized[1])**2).mean()
 
-plt.gcf().gca().add_artist(plt.Circle(centre, r))
+# Plot
+import matplotlib.pyplot as plt
+plt.plot(x, y, 'r*')
+plt.gcf().gca().add_artist(plt.Circle(centre_optimized, r))
+plt.show()
