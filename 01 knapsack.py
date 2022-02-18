224b0877b61c770e77a08e872af7a1f984d38fab
heshameraqi <hesham.eraqi@gmail.com>
2022-02-18 18:58:29 +0200

    Update 01 knapsack.py

--- 01 knapsack.py
+++ 01 knapsack.py
@@ -10,8 +10,8 @@ import numpy as np
 def knap_sack(W, wt, val):
     V = np.zeros((len(wt)+1, W+1))
    
-    for c in range(1, V.shape[1]):
-        for r in range(1, V.shape[0]):
+    for r in range(1, V.shape[0]):
+        for c in range(1, V.shape[1]):
             V[r,c] = V[r-1,c] # Won't take it
             if wt[r-1] <= c: # For indexing
                 V[r,c] = max(V[r-1,c], V[r-1,c-wt[r-1]]+val[r-1]) # Leave it OR add it to the wight before adding it?
