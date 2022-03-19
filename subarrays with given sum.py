e2319f59fa582f587bf90739110622cf895504b7
heshameraqi <hesham.eraqi@gmail.com>
2022-03-19 19:16:12 +0200

    Update subarrays with given sum.py

--- subarrays with given sum.py
+++ subarrays with given sum.py
@@ -8,42 +8,56 @@
 #
 ################################################################################
 
-arr = [3, 2, 1, 3, 2, 5, 5, 6, 6, 5, 3, 3]
+arr = [3, 2, 1, 3, 2,  5, 5,   6, 6, 5, 3, 3]
 target = 6
 
 # -------------------------------------------------------------
 # O(n^2) Solution
+for s in range(len(arr)):
+    sum = arr[s]
+    e = s
+    if sum == target: # If single element sum to target
+        print("Target sum is in index %d"%(s))
+    while sum <= target and (e+1)<len(arr):
+        e += 1
+        sum += arr[e]
+        if sum == target:
+            print("Target sum between index %d and %d"%(s, e))
+
+# -------------------------------------------------------------
+# O(n) Solution: using a hash table by storing cumulative sum in another array
+#arr     = [3, 2, 1, 3, 2,  5,  5,  6,  6, 5, 3, 3]
+#acc_arr = [3, 5, 6, 9, 11, 16, 21, 27, ...]
+acc_arr = []
+acc_sum = 0
+H = {}
+for i in range(len(arr)):
+    acc_sum += arr[i]
+    acc_arr.append(acc_sum)
+    H[acc_sum] = i
+
+for i in range(len(acc_arr)):
+    searching = acc_arr[i] - target
+    if (searching in H) and (H[searching]<i):
+        print("Target sum between index %d and %d"%(H[searching]+1, i))
+    if acc_arr[i] == target: # special case if starting with first index can lead to target
+        print("Target sum between index 0 and %d"%(i))
+
+
+# -------------------------------------------------------------
+# O(n^2) Solution (another style)
 start = 0
 p_sum = 0
 for end in range(len(arr)):
     p_sum += arr[end]
     if (p_sum == target):
-        print("Indices from " + str(start) + " to " + str(end))
+        print("Target sum between index " + str(start) + " and " + str(end))
         p_sum -= arr[start]
         start += 1
     while (p_sum > target):
         p_sum -= arr[start]
         start += 1
     if (p_sum == target):  # To detect the special case if subarray length is equal to 1
-        print("Indices from " + str(start) + " to " + str(end))
+        print("Target sum between index " + str(start) + " and " + str(end))
         p_sum -= arr[start]
         start += 1
-
-print("----------------------------------------------------------")
-# O(n) Solution: using a hash table by storing cumulative sum in another array
-cum_sum = [0]
-p_sum = 0
-for i in range(len(arr)):
-    p_sum += arr[i]
-    cum_sum.append(p_sum)
-
-# Search if 'cum_sum[j] - cum_sum[i] = target' (j>i) happens similar to the 'two sums' problem searching if sum[i] + sum[j] = target
-h = {}
-for i in range(len(cum_sum)):
-    h[cum_sum[i]] = i
-
-for i in range(len(cum_sum)):
-    searching = target + cum_sum[i]
-    if searching in h:
-        if h[searching] > i:
-            print("Indices from " + str(i) + " to " + str(h[searching]-1))
