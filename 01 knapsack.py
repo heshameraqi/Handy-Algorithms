ea3c6799c4c5a82a36f69899ef3fe2c3498880d6
heshameraqi <hesham.eraqi@gmail.com>
2022-03-20 00:25:49 +0200

    Update 01 knapsack.py

--- 01 knapsack.py
+++ 01 knapsack.py
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# Copyright (c) 2020
+# Copyright (c) 2022
 # Authors:
 #  Hesham M. Eraqi (heraqi@aucegypt.edu)
 #
@@ -8,16 +8,19 @@
 import numpy as np
 
 def knap_sack(W, wt, val):
-    V = np.zeros((len(wt)+1, W+1))
-   
-    for r in range(1, V.shape[0]):
-        for c in range(1, V.shape[1]):
-            V[r,c] = V[r-1,c] # Won't take it
-            if wt[r-1] <= c: # For indexing
-                V[r,c] = max(V[r-1,c], V[r-1,c-wt[r-1]]+val[r-1]) # Leave it OR add it to the wight before adding it?
-     
-    print (V)
-    return V[-1][-1]
+    v_mat = np.zeros((W+1, len(wt)+1))
+
+    for r in range(1, v_mat.shape[0]):
+        for c in range(1, v_mat.shape[1]):
+            v_mat[r,c] = v_mat[r, c-1] # Won't take it
+            if r-wt[c-1] >= 0: # For indexing protection
+                v_take = v_mat[r-wt[c-1], c-1] + val[c-1] # Value if we take it
+                if v_take > v_mat[r,c]: 
+                    v_mat[r,c] = v_take
+
+    print(v_mat)
+    return v_mat[-1,-1]
+
 
 val = [6, 10, 12] 
 wt = [1, 2, 3] 
